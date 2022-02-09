02ac04a5b7c0dbeb3059c44c1eab9d6171da9acb
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-02-09 12:18:54 -0500

    remove nil impl, convert event type <=> char

--- src/event_type.ml
+++ src/event_type.ml
@@ -19,4 +19,40 @@ type t =
   | D (** object: destroyed *)
   | M (** metadata *)
 
+let to_char = function
+  | B -> 'B'
+  | E -> 'E'
+  | X -> 'X'
+  | I -> 'I'
+  | C -> 'C'
+  | P -> 'P'
+  | A_b -> 'b'
+  | A_n -> 'n'
+  | A_e -> 'e'
+  | F_s -> 's'
+  | F_t -> 't'
+  | F_f -> 'f'
+  | N -> 'N'
+  | O -> 'O'
+  | D -> 'D'
+  | M -> 'M'
+
+let of_char = function
+  | 'B' -> B
+  | 'E' -> E
+  | 'X' -> X
+  | 'I' -> I
+  | 'C' -> C
+  | 'P' -> P
+  | 'b' -> A_b
+  | 'n' -> A_n
+  | 'e' -> A_e
+  | 's' -> F_s
+  | 't' -> F_t
+  | 'f' -> F_f
+  | 'N' -> N
+  | 'O' -> O
+  | 'D' -> D
+  | 'M' -> M
+  | c -> invalid_arg (Printf.sprintf "catapult: unknown event type %C" c)
 
--- src/nil_impl.mli
+++
@@ -1,6 +0,0 @@
-
-(** Nil implementation.
-
-    This does not setup any backend. *)
-
-include Impl.S
