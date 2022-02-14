ced778c614a74be79c3cbfbb7b21802074eb65f0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-02-14 12:48:21 -0500

    example: add `-j ` to the heavy example, to try multiprocess traces

--- examples/heavy/heavy.ml
+++ examples/heavy/heavy.ml
@@ -44,28 +44,50 @@ let () =
   let mode = ref File in
   let file = ref "trace.json" in
   let addr = ref Catapult_client.default_endpoint in
+  let j = ref 1 in
   let db = ref "trace.db" in
+  let trace_id = ref "" in
   let sync = ref `NORMAL in
+  let worker = ref false in
   let opts = [
+    "-j", Arg.Set_int j, " number of processes";
     "-n", Arg.Set_int n, " number of iterations";
     "-o", Arg.Set_string file, " output file";
     "--mode", Arg.Symbol (["net"; "file"; "db"], (fun s->mode := mode_of_str s)), " serialization mode";
+    "--worker", Arg.Set worker, " act as a worker";
     "--db", Arg.String (fun s -> mode:= Db; db := s), " set trace database file";
     "--db-sync", Arg.Symbol (["normal";"off";"full"], fun s -> sync := sync_of_str s), " set level of sync for db";
+    "--trace-id", Arg.Set_string trace_id, " set trace ID";
     "--addr",
     Arg.String (fun s -> mode:=Net; addr := Catapult_client.Endpoint_address.of_string_exn s),
     " network address";
   ] |> Arg.align in
   Arg.parse opts (fun _ -> ()) "heavy";
 
+  if !worker then (
+    Catapult_sqlite.set_multiproc true;
+  ) else if not !worker && !j>1 then (
+    Catapult_sqlite.set_multiproc true;
+    let bin_name = Sys.executable_name in
+    for _k = 2 to !j do
+      let _p = Unix.open_process_args_out bin_name
+          (Array.of_list (bin_name :: "--worker" ::
+                          Array.to_list (Array.sub Sys.argv 1 (Array.length Sys.argv-1))))
+      in
+      ignore (_p : out_channel)
+    done;
+  );
+
   let run () = run !n in
   begin match !mode with
     | Net ->
       Printf.printf "use net client %s\n%!" (Catapult_client.Endpoint_address.to_string !addr);
+      if !trace_id <>"" then Catapult_client.set_trace_id !trace_id;
       Catapult_client.set_endpoint !addr;
       Catapult_client.with_setup run
     | Db ->
       Printf.printf "use sqlite backend %s\n%!" !db;
+      if !trace_id <>"" then Catapult_sqlite.set_trace_id !trace_id;
       Catapult_sqlite.set_file !db;
       Catapult_sqlite.set_sqlite_sync !sync;
       Catapult_sqlite.with_setup run
