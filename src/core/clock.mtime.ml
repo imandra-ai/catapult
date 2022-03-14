370ed5e7f00685eae938111eeb588662580c67c2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-14 14:42:46 -0400

    inherit timestamp for start of trace, for multi-process traces

--- src/core/clock.mtime.ml
+++ src/core/clock.mtime.ml
@@ -1,5 +1,13 @@
 
-let program_start = Mtime_clock.now()
+let program_start =
+  try Sys.getenv "TRACE_START" |> Int64.of_string |> Mtime.of_uint64_ns
+  with _ ->
+    let t = Mtime_clock.now() in
+    (* children need to inherit that *)
+    Unix.putenv "TRACE_START" (Int64.to_string @@ Mtime.to_uint64_ns t);
+    t
+
+
 
 (* main access to the clock *)
 let[@inline] now_us () : float =
