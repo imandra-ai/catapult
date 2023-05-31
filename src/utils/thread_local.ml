aadab3c4c61c6524101a281a682fcd3416e13ec6
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-05-31 16:45:28 -0400

    perf(thread-local): use multiple maps
    to access the TL state for a thread with tid=n, we access the map at
    index `n & 0xf` in the array. Within this map the key is `n >> 4`.
    This should reduce contention.

--- src/utils/thread_local.ml
+++ src/utils/thread_local.ml
@@ -11,55 +11,80 @@ end)
 type 'a value = { value: 'a } [@@unboxed]
 
 type 'a t = {
-  map: 'a value Int_map.t Atomic.t;
+  maps: 'a value Int_map.t Atomic.t array;
   init: t_id:int -> 'a;
   close: 'a -> unit;
 }
 (** A thread-local map. *)
 
-let[@inline] modify_map_ ~f (self : _ t) =
+let[@inline] modify_map_ ~f (self : _ t) i =
+  let map = self.maps.(i) in
   while
-    not
-      (let cur = Atomic.get self.map in
-       let new_ = f cur in
-       Atomic.compare_and_set self.map cur new_)
+    let cur = Atomic.get map in
+    let new_ = f cur in
+    not (Atomic.compare_and_set map cur new_)
   do
     ()
   done
 
-let size self = Int_map.cardinal (Atomic.get self.map)
+let size_map m = Int_map.cardinal (Atomic.get m)
+
+let size self =
+  let n = ref 0 in
+  Array.iter (fun m -> n := !n + size_map m) self.maps;
+  !n
+
+let mask_ = 0b1111
+let n_bits_ = 4
 
 let remove (self : _ t) ~t_id =
-  let m = Atomic.get self.map in
-  match Int_map.find_opt t_id m with
+  let offset = t_id land mask_ in
+  let key = t_id lsl n_bits_ in
+  let m = Atomic.get self.maps.(offset) in
+  match Int_map.find_opt key m with
   | None -> ()
   | Some value ->
-    modify_map_ self ~f:(fun m -> Int_map.remove t_id m);
+    modify_map_ self offset ~f:(fun m -> Int_map.remove key m);
     self.close value.value
+
+let create_ self t t_id =
+  let v = { value = self.init ~t_id } in
+  let offset = t_id land mask_ in
+  let key = t_id lsr n_bits_ in
+  modify_map_ self offset ~f:(fun m -> Int_map.add key v m);
+  Gc.finalise (fun _ -> remove self ~t_id) t;
+  v.value
 
 let get_or_create self : 'a =
   let t = Thread.self () in
   let t_id = Thread.id t in
-
-  let m = Atomic.get self.map in
-  match Int_map.find_opt t_id m with
+  let offset = t_id land mask_ in
+  let key = t_id lsr n_bits_ in
+  let m = Atomic.get self.maps.(offset) in
+  match Int_map.find_opt key m with
   | Some v -> v.value
-  | None ->
-    let v = { value = self.init ~t_id } in
-    modify_map_ self ~f:(fun m -> Int_map.add t_id v m);
-
-    Gc.finalise (fun _ -> remove self ~t_id) t;
-    v.value
+  | None -> create_ self t t_id
 
 let iter ~f self =
-  let m = Atomic.get self.map in
-  Int_map.iter (fun _ v -> f v.value) m
+  for i = 0 to mask_ do
+    let m = Atomic.get self.maps.(i) in
+    Int_map.iter (fun _ v -> f v.value) m
+  done
 
 let clear self =
-  let m = Atomic.exchange self.map Int_map.empty in
-  Int_map.iter (fun _ v -> self.close v.value) m
+  for i = 0 to mask_ do
+    let m = Atomic.exchange self.maps.(i) Int_map.empty in
+    Int_map.iter (fun _ v -> self.close v.value) m
+  done
 
 let create ~init ~close () : _ t =
-  let m = { map = Atomic.make Int_map.empty; init; close } in
+  let n_maps = 1 lsl n_bits_ in
+  let m =
+    {
+      maps = Array.init n_maps (fun _ -> Atomic.make Int_map.empty);
+      init;
+      close;
+    }
+  in
   Gc.finalise clear m;
   m
