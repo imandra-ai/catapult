5aa6b207f6df6780e06dfe4b391c704491ef33e3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-05-31 16:45:20 -0400

    add test for Thread_local in utils

---
+++ tests/t_thread_local.ml
@@ -0,0 +1,32 @@
+open Catapult_utils
+module TL = Thread_local
+
+let tl : int ref TL.t =
+  TL.create ~init:(fun ~t_id:_ -> ref 0) ~close:(fun _ -> ()) ()
+
+let () =
+  Printf.printf "start\n%!";
+  let l = ref [] in
+  let mutex = Mutex.create () in
+
+  let run () =
+    for i = 1 to 1_000_000 do
+      let r = TL.get_or_create tl in
+      incr r
+    done;
+    let r = TL.get_or_create tl in
+    Mutex.lock mutex;
+    l := !r :: !l;
+    Mutex.unlock mutex
+  in
+
+  let threads = Array.init 90 (fun _ -> Thread.create run ()) in
+  Array.iter Thread.join threads;
+
+  let threads = Array.init 50 (fun _ -> Thread.create run ()) in
+  Array.iter Thread.join threads;
+
+  Printf.printf "stop (l: [%s])\n%!"
+    (String.concat "; " @@ List.map string_of_int !l);
+  assert (!l = List.init (50 + 90) (fun _ -> 1_000_000));
+  ()
