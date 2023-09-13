a588ddcb1d38463a4f5aa6b825e0e6daf7dd7761
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-09-12 22:55:17 -0400

    more helpers in client/sqlite

--- src/client/catapult_client.ml
+++ src/client/catapult_client.ml
@@ -9,6 +9,18 @@ let default_endpoint = Endpoint_address.default
 let with_conn = Connection.with_
 let backend_of_conn : Connection.t -> Catapult.backend = Backend.make
 
+(** Parse a remote address. *)
+let addr_of_string_exn : string -> Endpoint_address.t =
+  Endpoint_address.of_string_exn
+
 (** Obtain a trace collector from a network connection *)
 let trace_collector_of_conn : Connection.t -> Trace_core.collector =
  fun conn -> backend_of_conn conn |> Catapult.trace_collector_of_backend
+
+(** [with_ ~addr () f] runs [f()] in an environment where a connection
+     to [addr] has been established and is used to forward
+     tracing events to  the remote daemon. *)
+let with_ ~addr ?trace_id () (f : unit -> 'a) : 'a =
+  Connection.with_ ~addr ?trace_id () @@ fun conn ->
+  Trace_core.setup_collector (trace_collector_of_conn conn);
+  f ()
