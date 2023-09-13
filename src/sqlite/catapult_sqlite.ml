0686aac99e3111509ceeab4ca74b1f72183da73a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-09-12 22:55:17 -0400

    more helpers in client/sqlite

--- src/sqlite/catapult_sqlite.ml
+++ src/sqlite/catapult_sqlite.ml
@@ -12,3 +12,11 @@ let backend_of_writer : Writer.t -> Catapult.backend = Backend.make
 (** Turn a writer into a Trace collector. *)
 let trace_collector_of_writer : Writer.t -> Trace_core.collector =
  fun wr -> backend_of_writer wr |> Catapult.trace_collector_of_backend
+
+(** [with_ () f] runs [f()] in a scope where a connection to
+     a Sqlite DB has been established and is used to store
+     tracing events emitted from within [f ()]. *)
+let with_ ?sync ?append ?file ?trace_id ?dir () (f : unit -> 'a) : 'a =
+  Writer.with_ ?sync ?append ?file ?trace_id ?dir () @@ fun wr ->
+  Trace_core.setup_collector (trace_collector_of_writer wr);
+  f ()
